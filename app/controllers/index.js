3263cb34413c6eb236de624886585e3cd1d04f20
Konstantin Indjov <cinjoff@gmail.com>
2015-11-03 19:55:44 +0100

    Fix routing reference

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,8 +2,8 @@ var express = require('express')
     , router = express.Router();
 
 router.use('/', require('./Users'));
-router.use('/', require('./Group'));
-router.use('/', require('./Event'));
+router.use('/', require('./Groups'));
+router.use('/', require('./Events'));
 
 router.get('/', function(req, res) {
     res.send('Home page');
