bd289bdd6975ca57e7a35ac5e79335666a0d1cc0
Konstantin Indjov <cinjoff@gmail.com>
2015-12-01 19:34:03 +0100

    Implement & Refactor /events calls

--- app/controllers/Events.js
+++ app/controllers/Events.js
@@ -2,85 +2,73 @@ var express = require('express')
     , router = express.Router();
 
 
-var path, httpMethod;
+var path;
 var Events = require('./EventsService');
-
 
 path = '/events'.replace('{',':').replace('}','');
 router.route(path)
     .get(function (req, res) {
         var query = req.params.query;
 
-
         Events.index(query, function(err, result){
-            if (err) {
-                res.send({error: err});
-            }
-            res.send(result);
+            if (err)
+                res.json({error: err});
+            res.json(result);
         });
-
-        res.send({'error': { 'code' : '401', 'message' : 'Bad request.' }});
-
     });
 
 path = '/events'.replace('{',':').replace('}','');
 router.route(path)
     .post(function (req, res) {
-        var name = req.param('name').value;
-        var startime = req.param('startime').value;
-        var endtime = req.param('endtime').value;
-        var location = req.param('location').value;
-        var description = req.param('description').value;
-        var pcnlist = req.param('pcnlist').value;
-
+        var name = req.param('name');
+        var startime = req.param('startime');
+        var endtime = req.param('endtime');
+        var location = req.param('location');
+        var description = req.param('description');
+        var pcnlist = JSON.parse(req.param('pcnlist'));
 
         Events.create(name, startime, endtime, location,  description, pcnlist, function(err, result){
-            if (err) {
-                res.send({error: err});
-            }
-            res.send(result);
-        });
-
-        res.send({'error': { 'code' : '401', 'message' : 'Bad request.' }});
-
-    });
-
-path = '/events/{id}'.replace('{',':').replace('}','');
-router.route(path)
-    .put(function (req, res) {
-        var id = req.param('id').value;
-        var name = req.param('name').value;
-        var startime = req.param('startime').value;
-        var duration = req.param('duration').value;
-        var description = req.param('description').value;
-        var location = req.param('location').value;
-
-
-        Events.update(id, name, startime, duration, description, location, function(err, result){
-            if (err) {
+            if (err)
                 res.json({error: err});
-            }
-            res.send(result);
+            res.json(result);
         });
-
-
     });
 
 path = '/events/{id}'.replace('{',':').replace('}','');
 router.route(path)
+    .get(function (req, res) {
+        var id = req.params.id;
+
+        Events.show(id, function(err, result){
+            if (err)
+                res.json({error: err});
+            res.json(result);
+        });
+    })
+
+    .put(function (req, res) {
+        var id = req.param('id');
+        var name = req.param('name');
+        var startTime = req.param('startTime');
+        var endTime = req.param('endTime');
+        var description = req.param('description');
+        var location = req.param('location');
+
+        Events.update(id, name, startTime, endTime, description, location, function(err, result){
+            if (err)
+                res.json({error: err});
+            res.json(result);
+        });
+    })
+
     .delete(function (req, res) {
         var id = req.param('id').value;
 
-
         Events.delete(id, function(err, result){
-            if (err) {
-                res.send({error: err});
-            }
-            res.send(result);
+            if (err)
+                res.json({error: err});
+            res.json(result);
         });
-
-        res.send({'error': { 'code' : '401', 'message' : 'Bad request.' }});   
-        
     });
 
 module.exports = router;
--- app/controllers/EventsService.js
+++ app/controllers/EventsService.js
@@ -4,26 +4,24 @@ UserModel = require('../models/User');
 module.exports = {
 
 	index: function(query, callback) {
-		//if error
-		callback(err, null);
-
-		//if valid
-		callback(null, result);
+		//TODO Implement query parameter
+		EventModel.find({}, '-_id id title description startTime endTime location pcnlist', function (err, events) {
+			if (err)
+				callback(err, null);
+			callback(null, events);
+		});
 	},
-
-	show: function(query, callback) {
-		//if error
-		callback(err, null);
-
-		//if valid
-		callback(null, result);
+	show: function(id, callback) {
+		EventModel.findOne({id: id}, '-_id id title description startTime endTime location pcnlist', function (err, event) {
+			if (err)
+				callback(err, null);
+			callback(null, event);
+		});
 	},
 
 	create: function(name, startime, endtime, location, description, pcnlist, callback) {
-		//TODO Create EventModel with the static information. Limit description to 140 characters
+		//Create event model
 		var event = new EventModel();
-		var user = new UserModel();
-
 		event.name = name;
 		event.startTime = startime;
 		event.endTime = endtime;
@@ -31,23 +29,42 @@ module.exports = {
 		event.description = description;
 		event.pcnlist = pcnlist;
 
-		//TODO create event and call callback
+		//Add the list of PCN numbers to the event list, setting the status as invited (default)
+		pcnlist.forEach(function (e){
+			event.pcnlist.push({pcn: e, status: "invited"});
+		});
+
+		console.log(pcnlist);
+
+		EventModel.create(event,function(err, res){
+			if (err)
+				callback(err, null);
+			callback(null, res);
+		});
+		//TODO Send push notifications with invitations to all the clients in the pcnlist
 	}, 
 
-	update: function(id, nameopt, startimeopt, durationopt, description, locationopt, callback) {
-		//if error
-		callback(err, null);
+	update: function(id, name, startTime, endTime, description, location, callback) {
+		var conditions = { id: id }
+			, update = { name: name, startTime: startTime, endTime: endTime, description: description, location: location}
+			, options = { multi: true };
 
-		//if valid
-		callback(null, result);
+		EventModel.update(conditions, update, options, function(err, numAffected){
+			if (err)
+				callback(err, null);
+
+			if (numAffected>0) {
+				callback(null, '{success: '+numAffected+' users changed');
+			}
+		});
 	}, 
 
 	delete: function(id, callback) {
-		//if error
-		callback(err, null);
-
-		//if valid
-		callback(null, result);
+		EventModel.findOneAndRemove({ id: id}, function(err, res) {
+			if (err)
+				callback(err, null);
+			callback(null, res);
+		});
 	}
 };
 
