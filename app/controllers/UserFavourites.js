2ab72fb02b9b43fd0b26bf6f65088c2ca6e9367a
Konstantin Indjov <cinjoff@gmail.com>
2015-12-01 19:48:01 +0100

    [WIP] Implement /user/:pcn/favourites calls

--- app/controllers/UserFavourites.js
+++ app/controllers/UserFavourites.js
@@ -1,62 +1,41 @@
 var express = require('express')
     , router = express.Router();
 
-
-var path, httpMethod;
+var path;
 var UserFavourites = require('./UserFavouritesService');
-
 
 path = '/users/{pcn}/favourites'.replace('{',':').replace('}','');
 router.route(path)
     .get(function (req, res) {
-        var pcn = req.param('pcn').value;
-        
+        var pcn = req.param('pcn');
 
         UserFavourites.index(pcn, function(err, result){
-            if (err) {
-                res.send({error: err});
-            }
-            res.send(result);
+            if (err)
+                res.json({error: err});
+            res.json(result);
         });
+    })
 
-        res.send({'error': { 'code' : '401', 'message' : 'Bad request.' }});   
-        
-    });
-
-path = '/users/{pcn}/favourites'.replace('{',':').replace('}','');
-router.route(path)
     .post(function (req, res) {
-        var pcn = req.param('pcn').value;
-        var pcnfavourite = req.param('pcnfavourite').value;
-        
+        var pcn = req.param('pcn');
+        var pcnfavourite = req.param('pcnfavourite');
 
         UserFavourites.create(pcn, pcnfavourite, function(err, result){
-            if (err) {
-                res.send({error: err});
-            }
-            res.send(result);
+            if (err)
+                res.json({error: err});
+            res.json(result);
         });
+    })
 
-        res.send({'error': { 'code' : '401', 'message' : 'Bad request.' }});   
-        
-    });
-
-path = '/users/{pcn}/favourites'.replace('{',':').replace('}','');
-router.route(path)
     .delete(function (req, res) {
-        var pcn = req.param('pcn').value;
-        var pcnfavourite = req.param('pcn').value;
-        
+        var pcn = req.param('pcn');
+        var pcnfavourite = req.param('pcn');
 
         UserFavourites.usersPcnFavouritesDelete(pcn, pcnfavourite, function(err, result){
-            if (err) {
-                res.send({error: err});
-            }
-            res.send(result);
+            if (err)
+                res.json({error: err});
+            res.json(result);
         });
-
-        res.send({'error': { 'code' : '401', 'message' : 'Bad request.' }});   
-        
     });
 
 module.exports = router;
--- app/controllers/UserFavouritesService.js
+++ app/controllers/UserFavouritesService.js
@@ -4,29 +4,43 @@ UserModel = require('../models/User');
 module.exports = {
 
 	index: function(pcn, callback) {
-		//if error
-		callback(err, null);
-
-		//if valid
-		callback(null, result);
+		//TODO Implement query parameter (find in any fields)
+		UserModel.find({pcn: pcn}, '-_id favourites', function (err, favs) {
+			if (err)
+				callback(err, null);
+			callback(null, favs);
+		});
 	}, 
 
 	create: function(pcn, pcnfavourite, callback) {
-		//if error
-		callback(err, null);
+		var conditions = { pcn: pcn }
+			, update = {$push: {favourites: pcnfavourite}}
+			, options = { multi: true };
 
-		//if valid
-		callback(null, result);
+		UserModel.findOneAndUpdate(conditions, update, options, function(err, numAffected){
+			if (err)
+				callback(err, null);
+
+			if (numAffected>0) {
+				callback(null, '{success: '+numAffected+' users changed');
+			}
+		});
 	}, 
 
 	delete: function(pcn, pcnfavourite, callback) {
-		//if error
-		callback(err, null);
+		var conditions = { pcn: pcn }
+			, update = {$pull: {favourites: pcnfavourite}}
+			, options = { multi: true };
 
-		//if valid
-		callback(null, result);
+		UserModel.findOneAndUpdate(conditions, update, options, function(err, numAffected){
+			if (err)
+				callback(err, null);
+
+			if (numAffected>0) {
+				callback(null, '{success: '+numAffected+' users changed');
+			}
+		});
 	}
-
-}
+};
 
 
