c33d31bc933a18cf18611890c0e59fc66182758a
Konstantin Indjov <cinjoff@gmail.com>
2015-12-01 19:46:11 +0100

    [WIP] Cleanup and refactor of /groups/:id/events

--- app/controllers/GroupEventsService.js
+++ app/controllers/GroupEventsService.js
@@ -1,16 +1,28 @@
 GroupModel = require('../models/Group');
+EventModel = require('../models/Event');
 
 
 module.exports = {
 
 	index: function(id, callback) {
-		//if error
-		callback(err, null);
+		GroupModel.findOne({id: id}, '-_id events', function (err, eventIds) {
+			if (err)
+				callback(err, null);
 
-		//if valid
-		callback(null, result);
+			// Get the list of event IDs returned for this group
+			var eventIDs = JSON.parse(eventIds).toArray();
+
+			EventModel.find()
+				.where('id')
+				.in(eventIDs)
+				.exec(function (err, events) {
+					if (err)
+						callback(err, null);
+
+					callback(null, events);
+				});
+		});
 	}
-
 };
 
 
