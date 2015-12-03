9dfd97c14c75d4cded28e1b81ec08fd0b1dbb08b
Konstantin Indjov <cinjoff@gmail.com>
2015-12-03 16:37:31 +0100

    Add initial implementation of chat

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var morgan      = require('morgan');
 
 var app        = express();                 // define our app using express
 
+
+
 //MongoDB connection
 var rawURI = 'mongodb://g2sadmin:G2SMONGOADMIN77@ds047524.mongolab.com:47524/go2study';
 var mongoURI = uriUtil.formatMongoose(rawURI);
@@ -29,7 +31,42 @@ var port = process.env.PORT || 8080;        // set our port
 app.use(morgan('dev'));
 app.use('/api', require('./app/controllers'));
 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+
+// START THE CHAT SERVER
+//CHAT
+var http = require('http').Server(app); // Used for publishing the chat service
+var io = require('socket.io')(http); // Socket.IO - used for the chat
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+//on chat message - emit to all users
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        socket.broadcast.emit('chat message', msg);
+    });
+});
+
+http.listen(8080, "127.0.0.1");
+
 // START THE SERVER
 // =============================================================================
-app.listen(port);
+//app.listen(port);
 console.log('Magic happens on port ' + port);
